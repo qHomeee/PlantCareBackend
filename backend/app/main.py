8f7602ccd78760a8ec18e8d520e0fddd4946ff2c
DmitriyZaulin <wanttodie37@gmail.com>
2026-05-18 14:42:17 +0300

    APP-03: rename importing variable from setting to settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,11 @@
 import fastapi
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.api import api_router
-from app.core.config import setting
+from app.core.config import settings
 
 app = fastapi.FastAPI(
-    title=setting.APP_NAME,
-    version = setting.APP_VERSION
+    title=settings.APP_NAME,
+    version = settings.APP_VERSION
 )
 
 app.add_middleware(
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_methods = ["*"],
     allow_headers = ["*"],
 )
-app.include_router(api_router,prefix=setting.API_V1_PREFIX)
+app.include_router(api_router,prefix=settings.API_V1_PREFIX)
 
 
 
