0a39b7e60bb9fdb90d7e686fa318e992ab27b498
DmitriyZaulin <wanttodie37@gmail.com>
2026-05-18 14:13:15 +0300

    APP-01: add base settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,18 @@
 import fastapi
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.api import api_router
+from app.core.config import setting
 
+app = fastapi.FastAPI(
+    title=setting.APP_NAME,
+    version = setting.APP_VERSION
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials=True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
 
