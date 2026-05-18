93b8fe3a8a6fcc8fbc0b61d78f8dd7beb3068ad7
DmitriyZaulin <wanttodie37@gmail.com>
2026-05-18 18:02:59 +0300

    DB-05: add new auth function for user

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 
-from app.core.security import hash_password
+from app.core.security import hash_password,verify_password
 from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate
 
@@ -35,3 +35,15 @@ def update_user(db: Session, user: User, user_data: UserUpdate) -> User:
     db.refresh(user)
 
     return user
+
+
+def authenficate_user(db:Session,email:str,password:str)->User|None:
+    user = get_user_by_email(db,email)
+
+    if not user: 
+        return None
+    
+    if not verify_password(password,user.hashed_password):
+        return None
+    return user
+
