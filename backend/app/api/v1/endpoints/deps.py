b4fbbd056cb24cc134e89efced9305704a9c9235
DmitriyZaulin <wanttodie37@gmail.com>
2026-05-19 00:16:51 +0300

    API-08: add getting current users token

---
+++ backend/app/api/v1/endpoints/deps.py
@@ -0,0 +1,40 @@
+from fastapi import Depends,HTTPException,status
+from fastapi.security import HTTPAuthorizationCredentials,HTTPBearer
+from sqlalchemy.orm import Session
+
+from app.core.database import get_db
+from app.core.security import decode_access_token
+from app.models.user import User
+from app.services.user_service import get_user_by_id
+
+
+bearer_scheme = HTTPBearer()
+
+
+def get_current_user(
+        credentials:HTTPAuthorizationCredentials =Depends(bearer_scheme),
+        db :Session = Depends(get_db),
+)-> User:
+    token = credentials.credentials
+    playload = decode_access_token(token)
+
+    if playload is None:
+        raise HTTPException(
+            status_code= status.HTTP_401_UNAUTHORIZED,
+            detail="Недействительный токен"
+        )
+    user_id = playload.get("sub")
+
+    if user_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Недействительный токен",
+        )
+    user = get_user_by_id(db, int(user_id))
+    
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Пользователь не найден"
+        )
