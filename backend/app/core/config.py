611afc5bbd1c1563dbcaf32db66dc83849e1265a
DmitriyZaulin <wanttodie37@gmail.com>
2026-05-18 14:37:04 +0300

    CONF-02: modify configs

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,22 +1,22 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-class Settings(BaseSettings):
-    APP_NAME:str = "PlantCare"
-    APP_VERSION: str = "1.0.0"
 
-    API_V1_PREFIX:str = "api/v1"
+class Settings(BaseSettings):
+    APP_NAME: str = "PlantCare"
+    APP_VERSION: str = "1.0.0"
+    API_V1_PREFIX: str = "/api/v1"
 
     DATABASE_URL: str
-    SECRET_KEY: str
 
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+
     PLANTNET_API_KEY: str
     PLANTNET_API_URL: str
-    
 
-    model_config  = SettingsConfigDict(env_file=".env",env_file_encoding="UTF-8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
-setting = Settings()
+settings = Settings()
