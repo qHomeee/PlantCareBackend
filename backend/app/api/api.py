8a695fd0e6a71a57a7a4f14b6a321856d0e79d52
DmitriyZaulin <wanttodie37@gmail.com>
2026-05-18 17:32:38 +0300

    API-03: add register route to main router

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -1,7 +1,18 @@
 import fastapi
+from api.v1.endpoints import auth
+
 
 api_router = fastapi.APIRouter()
 
 @api_router.get("/health")
 def health_check():
     return {"status":"ok"}
+
+
+api_router.include_router(
+    auth.router,
+    prefix="/auth",
+    tags =["Auth"]
+)
+
+
